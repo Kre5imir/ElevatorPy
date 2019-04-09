1ef8246a7601151a50dd50c12bd0c97b752d7ede
Kre5imir <33584041+Kre5imir@users.noreply.github.com>
2019-04-09 23:52:36 +0100

    Add files via upload

--- elevator_program.py
+++ elevator_program.py
@@ -6,10 +6,10 @@ class Building():
     no_of_users = int(input('enterr no of users'))
     no_of_floors = int(input('pls enter number of floors'))
 
-class User(Building):
+class User():
 
     def __init__(self, user_ID):
-        self.user_ID = user_ID
+        self.user_ID =  user_ID
         self.current_floor = randint(1, Building.no_of_floors)
         self.destination_floor = randint(1,Building.no_of_floors)
         self.downwards = self.current_floor > self.destination_floor
@@ -18,12 +18,12 @@ class User(Building):
 
             print("user id {} destination floor is {} and current floor is {}" \
                   .format(self.user_ID, self.destination_floor, self.current_floor))'''
-    def yrange(n):
+    '''def yrange(n):
         i = 0
         while i < n:
             yield i
             i += 1
-
+        '''
 class Elevator():
     down = False
     up = False
@@ -45,14 +45,12 @@ def main():
     user_list = B.building_users
     no_of_users = B.no_of_users
 
-    gen_ID = User.yrange(no_of_users)
-    U = User(gen_ID)
-
     E = Elevator()
     register_list = E.on_bord_list
 
+
     for i in range(no_of_users):
-        user_list.append(User(User.yrange(no_of_users)))
+        user_list.append(User(i))
 
 
     print(E.current_floor)
@@ -64,12 +62,11 @@ def main():
         if E.current_floor < i.current_floor:
             #E.up = True
            # while True:
-                if i.up == i.up:
-                    register_list.append(i.user_ID)
+                if i.up == E.up:
+                    register_list.append(i)
                 #False
 
-    for i in register_list:
-        print(i)
+    print(E.on_bord_list)
 if __name__== "__main__":
 
     main()
